344e9bac5a8026346516075adb20e9d14cd0ce94
cawtds <38510667+cawtds@users.noreply.github.com>
2026-02-13 19:19:36 +0100

    fix saveblock tests

--- src/save.c
+++ src/save.c
@@ -82,11 +82,6 @@ STATIC_ASSERT(sizeof(struct SaveBlock2) <= SECTOR_DATA_SIZE, SaveBlock2FreeSpace
 STATIC_ASSERT(sizeof(struct SaveBlock1) <= SECTOR_DATA_SIZE * (SECTOR_ID_SAVEBLOCK1_END - SECTOR_ID_SAVEBLOCK1_START + 1), SaveBlock1FreeSpace);
 STATIC_ASSERT(sizeof(struct PokemonStorage) <= SECTOR_DATA_SIZE * (SECTOR_ID_PKMN_STORAGE_END - SECTOR_ID_PKMN_STORAGE_START + 1), PokemonStorageFreeSpace);
 
-// char (*app)[sizeof(struct PlayersApprentice)] = 1;
-// char (*test3)[SAVE_BLOCK_3_CHUNK_SIZE * NUM_SECTORS_PER_SLOT - sizeof(struct SaveBlock3)] = 1;
-// char (*test2)[SECTOR_DATA_SIZE - sizeof(struct SaveBlock2)] = 1;
-// char (*test1)[SECTOR_DATA_SIZE * (SECTOR_ID_SAVEBLOCK1_END - SECTOR_ID_SAVEBLOCK1_START + 1) - sizeof(struct SaveBlock1)] = 1;
-
 // Sector num to begin writing save data. Sectors are rotated each time the game is saved. (possibly to avoid wear on flash memory?)
 COMMON_DATA u16 gLastWrittenSector = 0;
 COMMON_DATA u32 gLastSaveCounter = 0;
