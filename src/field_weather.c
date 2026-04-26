aa2951da7facdfddde81dfd8f5c4e5e3b3cc8047
cawtds <38510667+cawtds@users.noreply.github.com>
2026-04-26 15:24:46 +0200

    fix rocket hideout transition

--- src/field_weather.c
+++ src/field_weather.c
@@ -5,6 +5,7 @@
 #include "field_weather_effects.h"
 #include "field_weather.h"
 #include "gpu_regs.h"
+#include "map_preview_screen.h"
 #include "overworld.h"
 #include "palette.h"
 #include "quest_log.h"
@@ -304,7 +305,8 @@ static void Task_WeatherMain(u8 taskId)
 
 static void None_Init(void)
 {
-    Weather_SetBlendCoeffs(8, BASE_SHADOW_INTENSITY); // Indoor shadows
+    if (!ForestMapPreviewScreenIsRunning())
+        Weather_SetBlendCoeffs(8, BASE_SHADOW_INTENSITY); // Indoor shadows
     gWeatherPtr->noShadows = FALSE;
     gWeatherPtr->targetColorMapIndex = 0;
     gWeatherPtr->colorMapStepDelay = 0;
